8f61c13e9ff0b0c48f5c407c114210374577cb56
petrospgithub <petrakisp13@gmail.com>
2019-02-05 16:21:31 +0200

    spark benchmark

--- src/main/scala/benchmark/knnEvaluation.scala
+++ src/main/scala/benchmark/knnEvaluation.scala
@@ -48,7 +48,7 @@ object knnEvaluation {
       spark.sql("SELECT final.trajArowid, ToOrderedListBinary(final.distance, final.rowid, 1, final.traja, final.trajb) FROM ( " +
         " SELECT IndexStoreTrajKNN_binary(c.trajectory, d.tree, 40000.1, 604800, 604800, c.rowId, 'DTW', 'Euclidean', 1, 50, 0.1, 0 ) " +
         " FROM ( SELECT IndexTrajKNN_binary(a.trajectory,b.tree, 40000.1, 604800, 604800, a.rowId) FROM " +
-        " (SELECT * FROM trajectories_imis400_binary where rowId=9706626089284 ) as a JOIN partition_index_imis400_binary as b ) as c INNER JOIN " +
+        " (SELECT * FROM trajectories_imis400_binary where rowId=9706626089284 ) as a CROSS JOIN partition_index_imis400_binary as b ) as c INNER JOIN " +
         " index_imis400_binaryTraj as d ON (c.trajectory_id=d.id) ) as final GROUP BY final.trajArowid").show()
     })
 
