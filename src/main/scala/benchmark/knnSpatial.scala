09022b4186652020641862f1af22de962ad7436a
petrospgithub <petrakisp13@gmail.com>
2019-05-06 13:50:24 +0300

    spatial

---
+++ src/main/scala/benchmark/knnSpatial.scala
@@ -0,0 +1,65 @@
+package benchmark
+
+import org.apache.spark.sql.SparkSession
+
+object knnSpatial {
+
+  def main(args: Array[String]): Unit = {
+
+    val start=System.currentTimeMillis()
+
+    val spark = SparkSession.builder
+      .appName("knnEvaluation") //.master("local[*]")
+      .config("hive.metastore.uris", "thrift://83.212.100.24:9083")
+      .enableHiveSupport()
+      .getOrCreate()
+
+
+    val dist=args(0).toDouble
+    val time=args(1).toInt
+
+    spark.sparkContext.setLogLevel("WARN")
+    import spark.implicits._
+
+    spark.sql(" CREATE TEMPORARY FUNCTION IndexTrajKNN_arr AS 'di.thesis.hive.similarity.IndexKNN'")
+    spark.sql(" CREATE TEMPORARY FUNCTION IndexTrajKNN_binary AS 'di.thesis.hive.similarity.IndexKNNBinary'")
+
+
+    spark.sql(" CREATE TEMPORARY FUNCTION DTW_arr AS 'di.thesis.hive.similarity.DTWUDF'")
+    spark.sql(" CREATE TEMPORARY FUNCTION DTW_binary AS 'di.thesis.hive.similarity.DTWBinary'")
+
+
+    spark.sql(" CREATE TEMPORARY FUNCTION IndexStoreTrajKNN_binary AS 'di.thesis.hive.similarity.IndeKNNBinaryStoreTraj'")
+
+    spark.sql(" CREATE TEMPORARY FUNCTION ToOrderedList_arr AS 'di.thesis.hive.similarity.ToOrderedList'")
+    spark.sql(" CREATE TEMPORARY FUNCTION ToOrderedListBinary AS 'di.thesis.hive.similarity.ToOrderedListBinary'")
+    spark.sql(" CREATE TEMPORARY FUNCTION ToOrderedListBinarySpark AS 'di.thesis.hive.similarity.ToOrderedListBinarySpark'")
+
+    val arr=spark.sql("select rowId from trajectories_imis400_binary distribute by rand() sort by rand() limit 2").collect()
+
+    val id1=arr(0).getLong(0)
+    val id2=arr(1).getLong(0)
+
+
+    /* TODO add extreme time tolerance!!!
+    spark.time ({
+
+      spark.sql(" SELECT c.rowId, ToOrderedList_arr( DTW_arr(c.trajectory, trajectories_imis400_pid.trajectory, 50, 'Euclidean', 604800, 604800), trajectories_imis400_pid.rowId, '-k -1', c.trajectory, trajectories_imis400_pid.trajectory )" +
+        " FROM " +
+        " (SELECT IndexTrajKNN_arr(a.trajectory,b.tree, 40000.1, 604800, 604800, a.rowId) " +
+        " FROM (SELECT * FROM trajectories_imis400_pid WHERE rowId=" + id1 + ") as a JOIN partition_index_imis400 as b ) as c " +
+        " JOIN trajectories_imis400_pid ON (c.trajectory_id==trajectories_imis400_pid.pid) " +
+        " GROUP BY c.rowId ").collect()
+
+      spark.sql(" SELECT c.rowId, ToOrderedList_arr( DTW_arr(c.trajectory, trajectories_imis400_pid.trajectory, 50, 'Euclidean', 604800, 604800), trajectories_imis400_pid.rowId, '-k -1', c.trajectory, trajectories_imis400_pid.trajectory )" +
+        " FROM " +
+        " (SELECT  " +
+        " FROM (SELECT * FROM trajectories_imis400_pid WHERE rowId=" + id2 + ") as a JOIN partition_index_imis400 as b ) as c " +
+        " JOIN trajectories_imis400_pid ON (c.trajectory_id==trajectories_imis400_pid.pid) " +
+        " GROUP BY c.rowId ").collect()
+
+    })
+*/
+    spark.stop()
+  }
+}
---
+++ src/main/scala/benchmark/rangeSpatial.scala
@@ -0,0 +1,59 @@
+package benchmark
+
+import org.apache.spark.sql.SparkSession
+import utils.MbbSerialization
+
+object rangeSpatial {
+  def main(args: Array[String]): Unit = {
+
+    val start=System.currentTimeMillis()
+
+    val spark = SparkSession.builder
+      .appName("rangeEvaluation") //.master("local[*]")
+      .config("hive.metastore.uris", "thrift://83.212.100.24:9083")
+      .enableHiveSupport()
+      .getOrCreate()
+
+
+    spark.sql(" CREATE TEMPORARY FUNCTION ST_Intersects3D AS 'di.thesis.hive.stoperations.ST_Intersects3D' ")
+    spark.sql(" CREATE TEMPORARY FUNCTION MbbConstructor AS 'di.thesis.hive.mbb.MbbSTUDF' ")
+    spark.sql(" CREATE TEMPORARY FUNCTION ST_IndexIntersects AS 'di.thesis.hive.stoperations.IndexIntersects3D' ")
+
+    spark.sql(" CREATE TEMPORARY FUNCTION ST_IndexIntersectsBinary AS 'di.thesis.hive.stoperations.IndexIntersects3DBinary' ")
+    spark.sql(" CREATE TEMPORARY FUNCTION ST_Intersects3DBinary AS 'di.thesis.hive.stoperations.ST_Intersects3DBinary' ")
+    spark.sql(" CREATE TEMPORARY FUNCTION MbbConstructorBinary AS 'di.thesis.hive.mbb.MbbSTUDFBinary' ")
+    spark.sql(" CREATE TEMPORARY FUNCTION IndexIntersectsTraj AS 'di.thesis.hive.stoperations.IndexIntersectsTraj' ")
+
+    spark.sql(" CREATE TEMPORARY FUNCTION SP_TrajSPMBR AS 'di.thesis.hive.stoperations.SP_TrajSPMBR' ")
+
+    spark.sparkContext.setLogLevel("WARN")
+    import spark.implicits._
+
+
+    val arr=spark.sql("select box from index_imis400_temp_binary distribute by rand() sort by rand() limit 2").collect()//.head.getAs[Array[Byte]]("box")
+
+
+    val obj=arr(0).getAs[Array[Byte]]("box")
+    val obj2=arr(1).getAs[Array[Byte]]("box")
+
+
+    val env=Some(MbbSerialization.deserialize(obj.asInstanceOf[Array[Byte]]))
+    val env2=Some(MbbSerialization.deserialize(obj2.asInstanceOf[Array[Byte]]))
+
+
+    spark.time({
+
+     spark.sql(" SELECT * FROM " +
+        "(SELECT ST_IndexIntersectsBinary(MbbConstructorBinary( %s, %s, %s, %s, CAST(%s as BIGINT), CAST(%s as BIGINT) ),tree, 0.1, 0.1, 0.1, 0.1, 0, 0) FROM partition_index_imis400) as b INNER JOIN trajectories_imis400_binary_pid as a ON (b.trajectory_id=a.pid) ".format(env.get.getMinX, env.get.getMaxX, env.get.getMinY, env.get.getMaxY, env.get.getMinT, env.get.getMaxT) +
+        " WHERE ST_Intersects3DBinary(MbbConstructorBinary( %s, %s, %s, %s, CAST(%s as BIGINT), CAST(%s as BIGINT) ), trajectory, 0.1, 0.1, 0.1, 0.1, 0, 0)  ".format(env.get.getMinX, env.get.getMaxX, env.get.getMinY, env.get.getMaxY, env.get.getMinT, env.get.getMaxT)).collect()
+
+      spark.sql( " SELECT * FROM " +
+        "(SELECT ST_IndexIntersectsBinary(MbbConstructorBinary( %s, %s, %s, %s, CAST(%s as BIGINT), CAST(%s as BIGINT) ),tree, 0.1, 0.1, 0.1, 0.1, 0, 0) FROM partition_index_imis400) as b INNER JOIN trajectories_imis400_binary_pid as a ON (b.trajectory_id=a.pid) ".format(env2.get.getMinX, env2.get.getMaxX, env2.get.getMinY, env2.get.getMaxY, env2.get.getMinT, env2.get.getMaxT) +
+        " WHERE ST_Intersects3DBinary(MbbConstructorBinary( %s, %s, %s, %s, CAST(%s as BIGINT), CAST(%s as BIGINT) ), trajectory, 0.1, 0.1, 0.1, 0.1, 0, 0)  ".format(env2.get.getMinX, env2.get.getMaxX, env2.get.getMinY, env2.get.getMaxY, env2.get.getMinT, env2.get.getMaxT)).collect()
+
+
+    })
+
+    spark.stop()
+  }
+}
